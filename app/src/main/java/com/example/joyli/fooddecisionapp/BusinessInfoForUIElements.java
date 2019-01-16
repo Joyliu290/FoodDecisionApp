5eb64eb02d0b3acbdccca97ee60fb4ce3b7ab2da
Joyliu290 <joyliu290@hotmail.com>
2019-01-15 21:32:15 -0500

    Finished implementing class for interface that sets the UI elements for each restaurant

--- app/src/main/java/com/example/joyli/fooddecisionapp/BusinessInfoForUIElements.java
+++ app/src/main/java/com/example/joyli/fooddecisionapp/BusinessInfoForUIElements.java
@@ -1,13 +1,15 @@
 package com.example.joyli.fooddecisionapp;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by Joyli on 2019-01-13.
  */
 
 public interface BusinessInfoForUIElements {
-    void businessRatingUI (String rating);
-    void businessNameUI (String businessName);
-    void businessLocationUI(float businessLatitude, float businessLongitude);
-    void businessImageUI (String businessImage);
-    void businessPriceRangeUI(String businessPriceRange);
+    void businessRatingUI (String businessRating, ImageView businessRatingImage);
+    void businessNameUI (String businessName, TextView businessNameTextview);
+    void businessLocationUI(String businessLocation, TextView businessLocationTextview);
+    void businessImageUI (String businessImage, ImageView businessImageview);
 }
--- app/src/main/java/com/example/joyli/fooddecisionapp/RestaurantUIElements.java
+++ app/src/main/java/com/example/joyli/fooddecisionapp/RestaurantUIElements.java
@@ -1,32 +1,84 @@
 package com.example.joyli.fooddecisionapp;
 
+import android.content.Context;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
 /**
  * Created by Joyli on 2019-01-13.
  */
 
 public class RestaurantUIElements implements BusinessInfoForUIElements {
+
+    private Context context;
+
+    public RestaurantUIElements(Context context){
+        this.context = context;
+    }
+
     @Override
-    public void businessRatingUI(String rating) {
+    public void businessRatingUI(String businessRating, ImageView businessRatingImage) {
+        if (businessRating.equals("0")){
+            int drawableID= this.context.getResources().getIdentifier ("stars_regular_0","drawable", this.context.getPackageName());
+            businessRatingImage.setImageResource(drawableID);
+        }
+        else if (businessRating.equals("1")){
+            int drawableID= this.context.getResources().getIdentifier ("stars_regular_1","drawable", this.context.getPackageName());
+            businessRatingImage.setImageResource(drawableID);
+        }
+        else if (businessRating.equals("1.5")){
+            int drawableID= this.context.getResources().getIdentifier ("stars_regular_1_half","drawable", this.context.getPackageName());
+            businessRatingImage.setImageResource(drawableID);
+        }
+        else if (businessRating.equals("2")){
+            int drawableID= this.context.getResources().getIdentifier ("stars_regular_2","drawable", this.context.getPackageName());
+            businessRatingImage.setImageResource(drawableID);
+        }
+        else if (businessRating.equals("2.5")){
+            int drawableID= this.context.getResources().getIdentifier ("stars_regular_2_half","drawable", this.context.getPackageName());
+            businessRatingImage.setImageResource(drawableID);
+        }
+        else if (businessRating.equals("3")){
+            int drawableID= this.context.getResources().getIdentifier ("stars_regular_3","drawable", this.context.getPackageName());
+            businessRatingImage.setImageResource(drawableID);
+        }
+        else if (businessRating.equals("3.5")){
+            int drawableID= this.context.getResources().getIdentifier ("stars_regular_3_half","drawable", this.context.getPackageName());
+            businessRatingImage.setImageResource(drawableID);
+        }
+        else if (businessRating.equals("4")){
+            int drawableID= this.context.getResources().getIdentifier ("stars_regular_4","drawable", this.context.getPackageName());
+            businessRatingImage.setImageResource(drawableID);
+        }
+        else if (businessRating.equals("4.5")){
+            int drawableID= this.context.getResources().getIdentifier ("stars_regular_4_half","drawable", this.context.getPackageName());
+            businessRatingImage.setImageResource(drawableID);
+        }
+        else{
+            int drawableID= this.context.getResources().getIdentifier ("stars_regular_5","drawable", this.context.getPackageName());
+            businessRatingImage.setImageResource(drawableID);
+        }
 
     }
 
     @Override
-    public void businessNameUI(String businessName) {
-
+    public void businessNameUI(String businessName, TextView businessNameTextview) {
+        businessNameTextview.setText(businessName);
     }
 
     @Override
-    public void businessLocationUI(float businessLatitude, float businessLongitude) {
-
+    public void businessLocationUI(String businessLocation, TextView businessLocationTextview) {
+        businessLocationTextview.setText(businessLocation);
     }
 
     @Override
-    public void businessImageUI(String businessImage) {
-
+    public void businessImageUI(String businessImage, ImageView businessImageview) {
+        Picasso
+                .with(this.context)
+                .load(businessImage)
+                .into (businessImageview);
     }
 
-    @Override
-    public void businessPriceRangeUI(String businessPriceRange) {
-
-    }
 }
