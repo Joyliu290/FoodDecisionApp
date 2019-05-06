15cc7d7a0a9c7050b00534b5ebbf8544b660e466
Joyliu290 <joyliu290@hotmail.com>
2019-05-06 16:00:06 -0400

    Made yelp logo directly link to yelp.com to follow the API guideline, also updated MainActivity to contain more methods for readability

--- app/src/main/java/com/example/joyli/fooddecisionapp/MainActivity.java
+++ app/src/main/java/com/example/joyli/fooddecisionapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -38,7 +39,6 @@ import java.util.Random;
 
 import okhttp3.*;
 
-
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     TextView mRestaurantTitle, mRestaurantTitle2, mRestaurantTitle3, mLocation,mLocation2,mLocation3, mReview, mReview2, mReview3;
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     int i,j;
     ProgressBar mLoading, mLoading2, mLoading3;
     private TextView selectedPositionText;
+    private Restaurant restaurant1, restaurant2, restaurant3;
 
     private static final int MY_PERMISSION_REQUEST_CODE = 7171;
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 7172;
@@ -88,45 +89,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRestaurantTitle = (TextView) findViewById(R.id.foodName);
-        mRate = (ImageView) findViewById(R.id.rating);
-        mRate2 = (ImageView) findViewById(R.id.rating2);
-        mRate3 = (ImageView) findViewById(R.id.rating3);
-        mRestaurantTitle2 = (TextView) findViewById(R.id.foodName2);
-        mRestaurantTitle3 = (TextView) findViewById(R.id.foodName3);
-        mMainImage = (ImageView) findViewById(R.id.mainImage);
-        mMainImage2 = (ImageView) findViewById(R.id.mainImage2);
-        mMainImage3 = (ImageView) findViewById(R.id.mainImage3);
-        mLocation = (TextView) findViewById(R.id.location);
-        mLocation2 = (TextView) findViewById(R.id.location2);
-        mLocation3 = (TextView) findViewById(R.id.location3);
-        mReview = (TextView)findViewById(R.id.reviews);
-        mReview2 = (TextView)findViewById(R.id.reviews2);
-        mReview3 = (TextView)findViewById(R.id.reviews3);
-        mLogo = (ImageView)findViewById(R.id.logo);
-        mLogo2 = (ImageView)findViewById(R.id.logo2);
-        mLogo3 = (ImageView)findViewById(R.id.logo3);
-
-        selectedPositionText = (TextView) findViewById(R.id.selected_position_text);
-        mLoading = (ProgressBar) findViewById(R.id.progressBar2);
-        mLoading2 = (ProgressBar) findViewById(R.id.progressBar3);
-        mLoading3 = (ProgressBar) findViewById(R.id.progressBar4);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) { //RUNTIME REQUEST PERMISSION
-
-            ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-
-            }, MY_PERMISSION_REQUEST_CODE);
-
-        } else {
-            if (checkPlayServices()) {
-                buildGoogleApiClient();
-                createLocationRequest();
-            }
-        }
+        setAllUIElements();
+        checkLocationPermission();
 
         Button pressForFoodButton = (Button)findViewById(R.id.pressMe);
         pressForFoodButton.setOnClickListener(new View.OnClickListener() {
@@ -135,9 +99,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 // make current restaurant data invisible
                 clearAllRestaurantUI();
                 // make the progress bar visibile
-                mLoading.setVisibility(View.VISIBLE);
-                mLoading2.setVisibility(View.VISIBLE);
-                mLoading3.setVisibility(View.VISIBLE);
+               setAllProgressBarVisible();
                 // ideally, get the device's current latitude and longitude, right now just static
                 double currentLatitude = displayCurrentLatitudeLocationOfDevice();
                 double currentLongitude = displayCurrentLongitudeLocationOfDevice();
@@ -150,6 +112,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     public void getListOfRestaurants(double latitude, double longitude, String category){
         String apiKey = getApplicationContext().getString(R.string.yelp_api_key);
+
         // Creating GET request objects to make network calls to Yelp API to retrieve list of businesses under the category
         OkHttpClient client = new OkHttpClient();
         HttpUrl.Builder getRequestBuilder =
@@ -157,11 +120,12 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         getRequestBuilder.addQueryParameter("latitude", Double.toString(latitude));
         getRequestBuilder.addQueryParameter("longitude", Double.toString(longitude));
         getRequestBuilder.addQueryParameter("categories", category);
-        String getUrl = getRequestBuilder.build().toString();
+        getRequestBuilder.addQueryParameter("open_now", "true");
+        String requestUrl = getRequestBuilder.build().toString();
 
         Request getRequest = new Request.Builder()
                 .header("Authorization", "Bearer "+apiKey)
-                .url(getUrl)
+                .url(requestUrl)
                 .build();
         // Sending and receiving network calls
         client.newCall(getRequest).enqueue(new Callback() {
@@ -178,9 +142,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                     try {
                         JSONObject jsonObject = new JSONObject(response.body().string());
                         JSONArray businessesJsonArray = jsonObject.getJSONArray("businesses");
-                        Restaurant restaurant1 = new Restaurant(businessesJsonArray.getJSONObject(0), getApplicationContext(), mRate, mRestaurantTitle, mLocation, mReview, mMainImage);
-                        Restaurant restaurant2 = new Restaurant(businessesJsonArray.getJSONObject(1), getApplicationContext(), mRate2, mRestaurantTitle2, mLocation2, mReview2, mMainImage2);
-                        Restaurant restaurant3 = new Restaurant(businessesJsonArray.getJSONObject(2), getApplicationContext(), mRate3, mRestaurantTitle3, mLocation3, mReview3, mMainImage3);
+                        restaurant1 = new Restaurant(businessesJsonArray.getJSONObject(0), getApplicationContext(), mRate, mRestaurantTitle, mLocation, mReview, mMainImage);
+                        restaurant2 = new Restaurant(businessesJsonArray.getJSONObject(1), getApplicationContext(), mRate2, mRestaurantTitle2, mLocation2, mReview2, mMainImage2);
+                        restaurant3 = new Restaurant(businessesJsonArray.getJSONObject(2), getApplicationContext(), mRate3, mRestaurantTitle3, mLocation3, mReview3, mMainImage3);
 
                         setRestaurantData(restaurant1, restaurant2, restaurant3);
                     } catch (JSONException e) {
@@ -212,6 +176,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                     restaurant3.setBusinessRatingUI();
                     restaurant3.setBusinessNameUI();
                     restaurant3.setBusinessReviewCountUI();
+
+                    setOnClickYelpLogo();
                 }
             });
     }
@@ -263,6 +229,24 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
           return  "greek";
       }
   }
+
+    private void checkLocationPermission(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) { //RUNTIME REQUEST PERMISSION
+
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+
+            }, MY_PERMISSION_REQUEST_CODE);
+
+        } else {
+            if (checkPlayServices()) {
+                buildGoogleApiClient();
+                createLocationRequest();
+            }
+        }
+    }
 
     public double displayCurrentLatitudeLocationOfDevice() {
         //displays the latitude
@@ -353,12 +337,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onConnectionSuspended(int i) {
         mGoogleApiClient.connect();
-
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
     }
 
     @Override
@@ -366,6 +348,44 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         mLastLocation = location;
 
         displayCurrentLatitudeLocationOfDevice();
+    }
+
+    private void setAllUIElements(){
+        mRestaurantTitle = (TextView) findViewById(R.id.foodName);
+        mRate = (ImageView) findViewById(R.id.rating);
+        mRate2 = (ImageView) findViewById(R.id.rating2);
+        mRate3 = (ImageView) findViewById(R.id.rating3);
+        mRestaurantTitle2 = (TextView) findViewById(R.id.foodName2);
+        mRestaurantTitle3 = (TextView) findViewById(R.id.foodName3);
+        mMainImage = (ImageView) findViewById(R.id.mainImage);
+        mMainImage2 = (ImageView) findViewById(R.id.mainImage2);
+        mMainImage3 = (ImageView) findViewById(R.id.mainImage3);
+        mLocation = (TextView) findViewById(R.id.location);
+        mLocation2 = (TextView) findViewById(R.id.location2);
+        mLocation3 = (TextView) findViewById(R.id.location3);
+        mReview = (TextView)findViewById(R.id.reviews);
+        mReview2 = (TextView)findViewById(R.id.reviews2);
+        mReview3 = (TextView)findViewById(R.id.reviews3);
+        mLogo = (ImageView)findViewById(R.id.logo);
+        mLogo2 = (ImageView)findViewById(R.id.logo2);
+        mLogo3 = (ImageView)findViewById(R.id.logo3);
+
+        selectedPositionText = (TextView) findViewById(R.id.selected_position_text);
+        mLoading = (ProgressBar) findViewById(R.id.progressBar2);
+        mLoading2 = (ProgressBar) findViewById(R.id.progressBar3);
+        mLoading3 = (ProgressBar) findViewById(R.id.progressBar4);
+    }
+
+    public void setAllProgressBarVisible(){
+        mLoading.setVisibility(View.VISIBLE);
+        mLoading2.setVisibility(View.VISIBLE);
+        mLoading3.setVisibility(View.VISIBLE);
+    }
+
+    public void setAllProgressBarInvisible(){
+        mLoading.setVisibility(View.INVISIBLE);
+        mLoading2.setVisibility(View.INVISIBLE);
+        mLoading3.setVisibility(View.INVISIBLE);
     }
 
     public void clearAllRestaurantUI(){
@@ -387,12 +407,19 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     }
 
     public void showLogoAndMakeProgressBarInvisible(){
-        mLoading.setVisibility(View.INVISIBLE);
-        mLoading2.setVisibility(View.INVISIBLE);
-        mLoading3.setVisibility(View.INVISIBLE);
+        setAllProgressBarInvisible();
         mLogo.setVisibility(View.VISIBLE);
         mLogo2.setVisibility(View.VISIBLE);
         mLogo3.setVisibility(View.VISIBLE);
+    }
+
+    public void setOnClickYelpLogo(){
+        mLogo.setOnClickListener((View view) -> {Intent browserYelpWebpage= new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.yelp.com"));
+            startActivity(browserYelpWebpage);});
+        mLogo2.setOnClickListener((View view)->{Intent browserYelpWebpage= new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.yelp.com"));
+        startActivity(browserYelpWebpage);});
+        mLogo3.setOnClickListener((View view)->{Intent browserYelpWebpage= new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.yelp.com"));
+            startActivity(browserYelpWebpage);});
     }
 }
 
